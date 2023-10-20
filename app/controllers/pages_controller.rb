6fcdd2a886e8bae33f20358712a1d72fb66ada76
Georges Guy Gustinvil <georgeslyangus@gmail.com>
2023-10-20 17:17:57 -0300

    Show message when lawyer doesn't exist in the database

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,16 +1,17 @@
 class PagesController < ApplicationController
     def home
+        @searching = false
     end 
 
     def searches
         if params[:query].present?
-            @lawyer = Pessoa.where("nome LIKE ?", "#{params[:query].downcase}")
-            print "lawyer:#{@lawyer.count}"
-            if @lawyer.count > 0
+            @query = params[:query]
+            @searching = true
+            @lawyers = Pessoa.where("nome LIKE ?", "#{params[:query].downcase}")
+            if @lawyers.count > 0
                 @processes = Processo.joins("INNER JOIN pessoas ON pessoas.id = processos.advogado_id WHERE pessoas.nome LIKE '#{params[:query].downcase}'")
-                @query = params[:query]
             end
-            render "home" 
-        end   
+        end 
+        render "home" 
     end
 end
