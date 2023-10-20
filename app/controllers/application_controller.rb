c5d927fc3ebc6f309936617c38ab74fb06d46617
Georges Guy Gustinvil <georgeslyangus@gmail.com>
2023-10-20 15:53:10 -0300

    Query all processes via input

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,6 @@
 class ApplicationController < ActionController::Base
+    helper_method :search_client
+    def search_client(id)
+        return @client = Pessoa.find(id)
+    end
 end
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,13 +4,13 @@ class PagesController < ApplicationController
 
     def searches
         if params[:query].present?
-            #@advogado= Pessoa.where("name LIKE ?", "%#{params[:query].downcase}%")
-            @processos = Processo.joins("INNER JOIN pessoas ON pessoas.id = processos.advogado_id WHERE pessoas.nome LIKE %#{params[:query].downcase}%")
-
-            
-            #Processo.joins(:Pessoa).where("name LIKE ?", "%#{params[:query].downcase}%").select('processos.*')
-            render "home"
-            #@query = Processos.
+            @lawyer = Pessoa.where("nome LIKE ?", "#{params[:query].downcase}")
+            print "lawyer:#{@lawyer.count}"
+            if @lawyer.count > 0
+                @processes = Processo.joins("INNER JOIN pessoas ON pessoas.id = processos.advogado_id WHERE pessoas.nome LIKE '#{params[:query].downcase}'")
+                @query = params[:query]
+            end
+            render "home" 
         end   
     end
 end
