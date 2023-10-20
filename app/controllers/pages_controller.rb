dff87167b5f67f13bd408976646b5cf822675690
Georges Guy Gustinvil <georgeslyangus@gmail.com>
2023-10-19 23:36:33 -0300

    Creating the search form

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,16 @@
 class PagesController < ApplicationController
     def home
     end 
+
+    def searches
+        if params[:query].present?
+            #@advogado= Pessoa.where("name LIKE ?", "%#{params[:query].downcase}%")
+            @processos = Processo.joins("INNER JOIN pessoas ON pessoas.id = processos.advogado_id WHERE pessoas.nome LIKE %#{params[:query].downcase}%")
+
+            
+            #Processo.joins(:Pessoa).where("name LIKE ?", "%#{params[:query].downcase}%").select('processos.*')
+            render "home"
+            #@query = Processos.
+        end   
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,5 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   root "pages#home"
 
-  get 'search', to: 'searches#search_processes'
+  get 'search', to: 'pages#searches'
 end
